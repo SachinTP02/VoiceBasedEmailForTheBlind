648e886c712d80eb3680a39e92387004d8f03410
Sachin T P <sachintp1209@gmail.com>
2024-03-11 14:20:51 +0530

    First Commit - Created the skeleton

---
+++ mysite/homepage/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import render, redirect
+from . import forms
+from .models import Details
+from .models import Compose
+import imaplib,email
+from gtts import gTTS
+import os
+from playsound import playsound
+from django.http import HttpResponse
+import speech_recognition as sr
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from django.http import JsonResponse
+import re
+from django.core.mail import send_mail
+
+file = "good"
+i="0"
+passwrd = ""
+addr = ""
+item =""
+subject = ""
+body = ""
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+imap_url = 'imap.gmail.com'
+conn = imaplib.IMAP4_SSL(imap_url)
+attachment_dir = 'C:/Users/SACHIN/OneDrive/Desktop/'
